63a474e5a11daa7439e53e0fdd94084774216df7
spacem <spacem>
2021-08-29 11:56:23 +1000

    Change length to duration in seconds

--- src/resolvers.js
+++ src/resolvers.js
@@ -41,10 +41,15 @@ const resolvers = {
       return dataSources.trackAPI.getAuthor(authorId);
     },
 
+    durationInSeconds: ({ length }) => length,
+
     modules: ({ id }, _, { dataSources }) => {
       return dataSources.trackAPI.getTrackModules(id);
     },
   },
+  Module: {
+    durationInSeconds: ({ length }) => length,
+  }
 };
 
 module.exports = resolvers;
